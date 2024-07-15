48d9b21d89d48836ce510292129edea62b4cb4e6
Denis Espanhol <denisgfespanhol@gmail.com>
2024-07-15 14:25:23 -0300

    Add Collectable Script and Coins Prefabs

---
+++ RunMobile/Assets/Scripts/Collectables/CollectableBase.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableBase : MonoBehaviour
+{
+    [SerializeField] private string tagToCompare = "Player";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(tagToCompare)) Collect();
+    }
+
+    private void Collect()
+    {
+        gameObject.SetActive(false);
+    }
+}
