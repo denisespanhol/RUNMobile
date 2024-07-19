4fdc38881ea0fc6ee94ef5e7eb75cd6a34fe21dd
Denis Espanhol <denisgfespanhol@gmail.com>
2024-07-19 01:35:08 -0300

    Creating Animator Manager Script

---
+++ RunMobile/Assets/Scripts/Managers/AnimatorManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorManager : MonoBehaviour
+{
+    [SerializeField] Animator _animator;
+    [SerializeField] List<AnimatorSetup> _animatorSetups;
+
+    public enum AnimationType
+    {
+        IDLE,
+        RUN,
+        DEATH,
+        VICTORY
+    }
+
+    public void Play(AnimationType type, float currentSpeedFactor = 1)
+    {
+        _animatorSetups.ForEach(setup => { if (setup.animationType == type) _animator.SetTrigger(setup.trigger); _animator.speed = setup.speed * currentSpeedFactor; } );
+    }
+
+}
+
+[System.Serializable]
+public class AnimatorSetup
+{
+    public AnimatorManager.AnimationType animationType;
+    public string trigger;
+    public float speed = 1f;
+}
